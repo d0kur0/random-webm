74582291a6e512ae8f0b1db31b5e3e9f26e1ec9d
d0kur0 <dev.dokuro@gmail.com>
2019-10-05 18:46:41 +0300

    Add initialie daemono

--- app/server/daemon/index.php
+++ app/server/daemon/index.php
@@ -1,3 +1,20 @@
 <?php
 require_once("../vendor/autoload.php");
+define('DOMAIN', '2ch.hk');
 
+try {
+    $di = new DI\Container();
+
+    $boardsCollection = $di
+        ->get('App\Daemon\CollectBoards')
+        ->getBoards();
+
+    if (!$boardsCollection) {
+        throw new \Exception('The board collection was empty');
+    }
+
+    var_dump($boardsCollection);
+
+} catch (\Exception $exception) {
+
+}
