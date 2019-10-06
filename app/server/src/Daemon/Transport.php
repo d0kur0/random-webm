b17f2a567724857ca62bdab031c69120e8d22134
d0kur0 <dev.dokuro@gmail.com>
2019-10-06 03:15:59 +0300

    Implement getBoards page and add dependency: transportSettings

--- app/server/src/Daemon/Transport.php
+++ app/server/src/Daemon/Transport.php
@@ -1,29 +1,49 @@
 <?php
 namespace App\Daemon;
-use App\Daemon\ApiTransportConfigs;
 use \GuzzleHttp\Client as HttpClient,
-    \App\Daemon\ApiTransportConfigs;
+    \App\Daemon\TransportSettings;
 
-class ApiTransport
+class Transport
 {
     private $httpClient;
-    private $apiTransportConfigs;
+    private $transportSettings;
 
-    public function __construct (ApiTransportConfigs $apiTransportConfigs, HttpClient $httpClient)
+    public function __construct (TransportSettings $transportSettings, HttpClient $httpClient)
     {
         $this->httpClient = $httpClient;
-        $this->$apiTransportConfigs = $apiTransportConfigs;
-
-        var_dump($httpClient);
+        $this->transportSettings = $transportSettings;
     }
 
-    public function getBoardsPage ()
+    public function getBoards (): array
     {
-        $response = $this->client->request('GET', $this->getUri('/'));
+        $response = $this
+            ->httpClient
+            ->get($this->getUri('boards.json'));
+
+        if (!$response->getStatusCode() === "200") {
+            throw new \Exception("The request returned a response code other than 200");
+        }
+
+        $boards = $response
+            ->getBody()
+            ->getContents();
+
+        $boards = json_decode($boards);
+
+        if (json_last_error() !== JSON_ERROR_NONE) {
+            throw new \Exception('Could not parse the JSON list of boards');
+        }
+
+        return $boards;
     }
 
     private function getUri ($path) {
-        $uri = "http://" . DOMAIN . "/" . $path;
-        return $uri;
+        return(
+            $this->transportSettings->getProtocol().
+            "://" .
+            $this->transportSettings->getDomain().
+            "/".
+            $path
+        );
     }
 }
