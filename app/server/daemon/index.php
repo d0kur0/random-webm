ec73e45b0ce1dc11beaaa28cb1ec2d73f86360df
d0kur0 <dev.dokuro@gmail.com>
2019-10-06 03:14:37 +0300

    Add TransportSettings service

--- app/server/daemon/index.php
+++ app/server/daemon/index.php
@@ -4,6 +4,15 @@ define('DOMAIN', '2ch.hk');
 
 try {
     $di = new DI\Container();
+    $di->set('App\Daemon\TransportSettings', function () {
+        $transportConfigs = require('./configs/transportSettings.php');
+
+        $transportSettings = new App\Daemon\TransportSettings();
+        $transportSettings->setDomain($transportConfigs['domain']);
+        $transportSettings->setProtocol($transportConfigs['protocol']);
+
+        return $transportSettings;
+    });
 
     $boardsCollection = $di
         ->get('App\Daemon\CollectBoards')
@@ -16,5 +25,6 @@ try {
     var_dump($boardsCollection);
 
 } catch (\Exception $exception) {
-
+    // TODO: Logging errors
+    echo $exception->getMessage();
 }
